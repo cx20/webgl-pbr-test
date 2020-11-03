e838725a208e334a984743c75053aea98a287fc0
cx20 <y-shimano@hondarer-soft.com>
2020-11-04 07:52:19 +0900

    Update Rhodonite PBR sample

--- examples/rhodonite/simple/index.js
+++ examples/rhodonite/simple/index.js
@@ -4,7 +4,7 @@ function generateEntity() {
     return entity;
 }
 
-function readySphereVerticesData() {
+function createSpherePrimitive(roughnessFactor, metallicFactor) {
     //let modelMaterial = Rn.MaterialHelper.createClassicUberMaterial();
 
     let isMorphing = false;
@@ -12,19 +12,19 @@ function readySphereVerticesData() {
     let isLighting = true;
     let alphaMode = {index:0, str: "OPAQUE"};
     let additionalName = undefined;
-    let maxMaterialInstanceNumber = 1;
+    let maxMaterialInstanceNumber = 25; // TODO: need to investigation out what the value is
     let modelMaterial = Rn.MaterialHelper.createPbrUberMaterial({
         isMorphing, isSkinning, isLighting, alphaMode,
         additionalName: additionalName, maxInstancesNumber: maxMaterialInstanceNumber
     });
 
-    let metallicFactor = 1.0;
-    let roughnessFactor = 1.0;
+    //let metallicFactor = 1.0;
+    //let roughnessFactor = 1.0;
     modelMaterial.setParameter(Rn.ShaderSemantics.MetallicRoughnessFactor, new Rn.Vector2(metallicFactor, roughnessFactor));
     modelMaterial.setParameter(Rn.ShaderSemantics.BaseColorFactor, new Rn.Vector4(1.0, 1.0, 1.0, 1.0));
 
     const primitive = new Rn.Sphere();
-    primitive.generate({ radius: 1, widthSegments: 40, heightSegments: 40, material: modelMaterial });
+    primitive.generate({ radius: 0.5, widthSegments: 24, heightSegments: 24, material: modelMaterial });
 
 /*
     const texture = new Rn.Texture();
@@ -58,25 +58,29 @@ promise.then(function() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    const primitiveSphere = readySphereVerticesData();
-
-    Rn.MeshRendererComponent.manualTransparentSids = [];
-
     const entities = [];
-    const mesh = new Rn.Mesh();
-    mesh.addPrimitive(primitiveSphere);
-    const entity = generateEntity();
+    
+    //       [Metal] ^ 1.0
+    //               |
+    //               |
+    //   [Non-metal] v 0.0             1.0
+    //                 <---------------->
+    //            [Smooth]            [Rough]
+    for(let r = 0.0; r <= 1.0; r += 0.25) {
+        for(let m = 0.0; m <= 1.0; m += 0.25) {
+            // setup material
+            const spherePrimitive = createSpherePrimitive(r, m);
+            const mesh = new Rn.Mesh();
+            mesh.addPrimitive(spherePrimitive);
+            const entity = generateEntity();
+            const meshComponent = entity.getComponent(Rn.MeshComponent);
 
-    entities.push(entity);
-    const meshComponent = entity.getComponent(Rn.MeshComponent);
-
-    meshComponent.setMesh(mesh);
-    entity.getTransform().toUpdateAllTransform = false;
-    //entity.getTransform().translate = new Rn.Vector3(1.5, 0, 0);
-
-    const startTime = Date.now();
-    let p = null;
-    let count = 0
+            meshComponent.setMesh(mesh);
+            entity.getTransform().toUpdateAllTransform = false;
+            entity.getTransform().translate = new Rn.Vector3((r-0.5)*4, (m-0.5)*4, 0);
+            entities.push(entity);
+        }
+    }
 
     // camera
     const cameraComponent = createCameraComponent();
@@ -85,24 +89,17 @@ promise.then(function() {
     cameraComponent.setFovyAndChangeFocalLength(45);
     cameraComponent.aspect = window.innerWidth / window.innerHeight;
     const cameraEntity = cameraComponent.entity;
-    cameraEntity.getTransform().translate = new Rn.Vector3(0, 0, 8);
+    cameraEntity.getTransform().translate = new Rn.Vector3(0, 0, 6.5);
 
     // Lights
     const entityRepository = Rn.EntityRepository.getInstance();
 
     // Lights
-/*  
-    const lightEntity1 = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.LightComponent])
-    lightEntity1.getTransform().translate = new Rn.Vector3(1.0, 1.0, 100000.0);
-    lightEntity1.getComponent(Rn.LightComponent).intensity = new Rn.Vector3(1, 1, 1);
-    lightEntity1.getComponent(Rn.LightComponent).type = Rn.LightType.Directional;
-    lightEntity1.getTransform().rotate = new Rn.Vector3(-Math.PI / 1.5, 0, Math.PI / 1.5);
-*/
-    const lightEntity2 = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.LightComponent])
-    lightEntity2.getTransform().translate = new Rn.Vector3(1.0, 1.0, 100000.0);
-    lightEntity2.getComponent(Rn.LightComponent).intensity = new Rn.Vector3(1, 1, 1);
-    lightEntity2.getComponent(Rn.LightComponent).type = Rn.LightType.Directional;
-    lightEntity2.getTransform().rotate = new Rn.Vector3(Math.PI / 1.5, 0, -Math.PI / 1.5);
+    const lightEntity = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.LightComponent])
+    //lightEntity.getTransform().translate = new Rn.Vector3(0.0, 0.0, 1.0);
+    lightEntity.getComponent(Rn.LightComponent).intensity = new Rn.Vector3(1, 1, 1);
+    lightEntity.getComponent(Rn.LightComponent).type = Rn.LightType.Directional;
+    lightEntity.getTransform().rotate = new Rn.Vector3(Math.PI / 2, 0, 0);
 
     // renderPass
     const renderPass = new Rn.RenderPass();
@@ -122,7 +119,7 @@ promise.then(function() {
         return cameraComponent;
     }
 
-    let axis = new Rn.Vector3(1, 1, 1);
+    const startTime = Date.now();
 
     const draw = function(time) {
 
@@ -133,7 +130,6 @@ promise.then(function() {
         gl.disable(gl.CULL_FACE); // TODO:
         system.process([expression]);
 
-        count++;
         requestAnimationFrame(draw);
     }
 
